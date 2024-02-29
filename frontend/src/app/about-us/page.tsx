4745cbcb440e9f8f4d6557ef1f8ca1a2485ac165
dlemay <dylantlemay@gmail.com>
2024-02-28 19:40:46 -0600

    Added a descriptive About us page

---
+++ frontend/src/app/about-us/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import CartPanel from '../../components/cart-components/CartPanel'
+
+import  Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+import Slider from '@mui/material/Slider';
+import Background from '@/components/general/Background';
+
+export default function Home() {
+  return (
+    <>
+    <Background />
+    <div className ="relative"> Yo what up it the Shibuya store 😎 We make all our clothes authentically, in a Chinese sweat sho- whoops I mean Shibuya. We are trendy and cool and want your mon- whoops I mean want to help you define your own style! Wow! 😎</div>
+    </>
+
+  )
+}
